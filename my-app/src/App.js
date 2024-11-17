8cea66fa1fb568365a79cad1925eb242735fc4dd
chaitanyapatiiil <patilchaitanyaom@gmail.com>
2024-11-17 18:54:07 +0530

    basic layout

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <h1>Welcome to MyApp</h1>
+        <p>This is the main content.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
